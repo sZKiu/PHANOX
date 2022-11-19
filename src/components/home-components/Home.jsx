4c8792be89d8285ecd32506d6fabd0acabb45d33
sZKiu <augustomendez544@gmail.com>
2022-11-19 13:57:15 -0300

    Some errors fixed

--- src/components/home-components/Home.jsx
+++ src/components/home-components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
   return (
     <div className="home">
       <Helmet>
-        <title>Home || PHANOX</title>
+        <title>PHANOX</title>
       </Helmet>
       
       <Nav />
